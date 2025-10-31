29860bdb10e8ce5fa7d39c7bbd5039d914c6b157
Waseem Ashraf <waseemashraf47@gmail.com>
2025-10-31 17:11:03 +0500

    Hello World

---
+++ src/cfehome/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import pathlib
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).parent
+
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    page_title = "Home Page"
+    context = {
+        'page_title': page_title,
+        'qs': qs,
+        'page_visit_count': page_qs.count(),
+        'percent_page_visits': (page_qs.count() / qs.count() * 100) if qs.count() > 0 else 0,
+        'total_visit_count': qs.count(),
+    }
+    path = request.path
+    # log the page visit
+    print("Logging page visit for path:", path)
+    PageVisit.objects.create(path=request.path)
+
+    
+    return render(request, "home.html", context)
+    
---
+++ src/visits/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class PageVisit(models.Model):
+    # db table to log page visits
+    # id -> automatically added primary key 1,2,3,4.......
+    path = models.TextField(blank=True, null=True) # Col
+    timestamp = models.DateTimeField(auto_now_add=True) # Col
+
+    # def __str__(self):
+    #     return f"Visited {self.path} at {self.timestamp}"
